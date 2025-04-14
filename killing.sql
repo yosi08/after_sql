28f0a4d2f14af3583ab10ae1610cf1e09a596245
권기범 <stress0803@gmail.com>
2025-04-14 19:31:20 +0900

    오도바이 수정2

--- killing.sql
+++ killing.sql
@@ -78,11 +78,17 @@ select why from Customer where custid >= 400;
 #1. target의 가격이 비싼 순으로 정렬하여 검색하세요.
 select * from Target order by price desc;
 #2. 날짜와 청부 이유를 내림차순으로 정렬한 결과를 출력하시오
-select d_day, why from Order_Kill ok join Customer c on ok.custid = c.custid order by d_day;
+select d_day, why from Order_Kill ok join Customer c on ok.custid = c.custid order by d_day desc
+;
 #3. 살해방법을 날짜순(오름차순)으로 정렬한 결과를 출력하시오.
 select * from Order_Kill order by d_day;
 #4. 타겟을 이름순으로 검색하시오.
 select * from Target order by tname;
+#1. 청구비용이 가장 비싼 것과 가장 싼 가격을 검색하시오.
+select price from Target
+#2. 가장 큰 현상금을 가진 타겟의 고객의 이름과 이유를 구하시오
+#3. 고객번호가 471인 고객의 타겟의 가격 평균
+#4. 청구비용이 가장 싼 타겟을 찾으시오.
 
 
 
