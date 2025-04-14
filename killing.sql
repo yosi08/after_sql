886768aa78eea5f7baa0d12e6684f1b37e41239b
권기범 <stress0803@gmail.com>
2025-04-14 20:31:29 +0900

    group by

--- killing.sql
+++ killing.sql
@@ -94,6 +94,19 @@ where t.price = (select max(price) from Target);
  where c.custid = 471;
 #4. 청구비용이 가장 싼 타겟을 찾으시오.
 select tname from Target where price = (select min(price) from Target);
+#1. 고객별 청부한 타겟의 수를 검색하시오.
+select custid,count(*) from Order_Kill group by custid;
+#2. 고객별 청부 금액이 50000원을 초과하는 청부를 몇번 넣었는지, 50000원 초과 청부의 가격 총 합을 구하시오
+select c.custid, sum(t.price), count(c.custid) from Customer c join Order_Kill o on c.custid = o.custid
+join Target t on o.targetid = t.targetid group by c.custid, t.price
+having t.price > 50000;
+#3. 살인 청부를 받은 횟수가 2번 이상인 타겟의 고객번호 최댓값을 구하시오.
+select max(custid) from Order_Kill group by targetid having count(*) >= 2;
+#4. 고객별로 요청한 타겟의 총 명수와 총 청부비용을 구하시오.
+select custid, sum(t.price), count(*) from Order_Kill o join Target t on o.targetid = t.targetid
+group by o.custid;
+
+
 
 
 
